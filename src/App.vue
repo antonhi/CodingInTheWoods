a042fa2a41407019c7d1881995c38f1907e76ced
anton <antonhpersonal@gmail.com>
2022-01-13 22:59:20 -0600

    Navigation Bar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <Navigation class="navigation"/>
   <router-view/>
 </template>
 
+<script>
+import Navigation from './components/Navigation.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navigation
+  }
+}
+</script>
+
 <style>
+.navigation {
+  height: 100px;
+  width: 100%;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  background-color: #F3F3F4;
   color: #2c3e50;
+}
+
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
 }
 
 #nav {
---
+++ src/components/Navigation.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+      <button>about</button>
+      <img src="@/assets/codeinthewoodslogo.png"/>
+      <button>patreon</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation'
+}
+</script>
+
+<style scoped>
+  .container {
+    display: flex;
+    gap: 12%;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+  }
+  .link {
+    object-fit: contain;
+    max-height: 20%;
+  }
+  img {
+    object-fit: contain;
+    max-height: 65%;
+  }
+  button {
+    background-color: transparent;
+    color: #14110f;
+    border-color: transparent;
+    font: 18px Avenir, sans-serif;
+    font-weight: bold;
+    transition: 0.5s ease-in;
+  }
+  button:hover {
+    color: #70A9A1;
+    transform: scale(110%, 110%);
+    cursor: pointer;
+  }
+</style>
